38882515cfb96566b487bc18697f0292d9561b35
Papaux-Lucien-EMF <Lucien.papaux@studentfr.ch>
2025-03-25 15:42:36 +0100

    avectrou fini

--- src/services/ListePersonne_AvecTrous.java
+++ src/services/ListePersonne_AvecTrous.java
@@ -3,32 +3,68 @@ package services;
 import models.Personne;
 
 public class ListePersonne_AvecTrous {
-
     public final static int NBRE_MAX_PERSONNES = 6;
 
     private Personne[] personnes;
 
     public ListePersonne_AvecTrous() {
-        // A faire !
+        this.personnes = new Personne[NBRE_MAX_PERSONNES];
     }
 
     public int getNombre() {
-        // A faire !
+        int nbre_personne = 0;
+
+        for (int i = 0; i < personnes.length; i++) {
+            if (personnes[i] != null) {
+                nbre_personne++;
+            }
+        }
+        return nbre_personne;
     }
 
     public void vider() {
-        // A faire !
+        int comptage = personnes.length;
+        for (int i = 0; i < personnes.length; i++) {
+            personnes[i] = null;
+            comptage--;
+        }
+        System.out.println("Nombre de personnes : " + comptage);
     }
 
     public boolean ajouter(Personne p) {
-        // A faire !
+        boolean ajoutReussi = false;
+
+        for (int i = 0; i < personnes.length; i++) {
+            if (personnes[i] == null) {
+                personnes[i] = p;
+                ajoutReussi = true;
+                break;
+            }
+        }
+        return ajoutReussi;
     }
 
     public boolean supprimer(Personne p) {
-        // A faire !
-    }
+        boolean supprReussi = false;
 
+        for (int i = 0; i < personnes.length; i++) {
+            if (personnes[i] != null) {
+                personnes[i] = null;
+                supprReussi = true;
+                break;
+            }
+        }
+        return supprReussi;
+    }
+    
     public void afficher() {
-        // A faire !
+        int comptage = 0;
+        for (int i = 0; i < personnes.length; i++) {
+            if (personnes[i] != null) {
+                System.out.println(personnes[i]);
+                comptage++;
+            }
+        }
+        System.out.println("Nombre de personnes : " + comptage);
     }
 }
